da4ae8b6d0956f68dcbce1bbcfac541668aa9228
wendel <wang_163_1260@163.com>
2021-04-19 18:17:43 +0800

    add authlogin

--- registerserver.go
+++ registerserver.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	log "github.com/EntropyPool/entropy-logger"
 	types "github.com/NpoolAccounting/service-register/types"
+	authapi "github.com/NpoolDevOps/fbc-auth-service/authapi"
+	authTypes "github.com/NpoolDevOps/fbc-auth-service/types"
 	etcdcli "github.com/NpoolDevOps/fbc-license-service/etcdcli"
 	httpdaemon "github.com/NpoolRD/http-daemon"
+	"github.com/google/uuid"
 	"io/ioutil"
 	"net/http"
 	_ "strings"
@@ -15,6 +18,8 @@ import (
 
 // etcd key
 const accountingDomain = "accounting.npool.top"
+
+const prometheusDomain = "prometheus.npool.top"
 
 type RegisterConfig struct {
 	Port int
@@ -64,6 +69,7 @@ func (s *RegisterServer) Run() error {
 }
 
 func (s *RegisterServer) ServiceRegisterRequest(w http.ResponseWriter, req *http.Request) (interface{}, string, int) {
+
 	// 解析请求参数
 	b, err := ioutil.ReadAll(req.Body)
 	if err != nil {
@@ -73,9 +79,29 @@ func (s *RegisterServer) ServiceRegisterRequest(w http.ResponseWriter, req *http
 	err = json.Unmarshal(b, &input)
 	fmt.Println(input)
 	if err != nil {
-		return nil, err.Error(), -2
+		return nil, err.Error(), -1
 	}
-	resp, err := etcdcli.Get(accountingDomain)
+	// 登录
+	// username password  login
+	userLoginInput := authTypes.UserLoginInput{
+		Username:  input.UserName,
+		Password:  input.Password,
+		AppId:     uuid.UUID{},
+		TargetUrl: "",
+	}
+	_, err = authapi.Login(userLoginInput)
+	if err != nil {
+		return nil, err.Error(), -1
+	}
+
+	// 判断 域名是否在域名数组里面
+	domainArr := []string{accountingDomain, prometheusDomain}
+	result := in(input.DomainName, domainArr)
+	// 不存在
+	if !result {
+		return nil, "domainName is no legal", -1
+	}
+	resp, err := etcdcli.Get(input.DomainName)
 	if err != nil && resp != nil {
 		log.Errorf(log.Fields{}, "cannot get %v: %v", accountingDomain, err)
 		return "", err.Error(), -1
@@ -98,7 +124,7 @@ func (s *RegisterServer) ServiceRegisterRequest(w http.ResponseWriter, req *http
 		// put json string
 		info, err := etcdcli.Put(input.DomainName, strs)
 		if err != nil {
-			return nil, err.Error(), -3
+			return nil, err.Error(), -1
 		}
 		return info, "success", 0
 	} else {
@@ -111,9 +137,18 @@ func (s *RegisterServer) ServiceRegisterRequest(w http.ResponseWriter, req *http
 
 		info, err := etcdcli.Put(input.DomainName, string(jsons))
 		if err != nil {
-			return nil, err.Error(), -3
+			return nil, err.Error(), -1
 		}
 		return info, "success", 0
 	}
 
+}
+
+func in(target string, str_array []string) bool {
+	for _, element := range str_array {
+		if target == element {
+			return true
+		}
+	}
+	return false
 }
--- types/types.go
+++ types/types.go
@@ -2,6 +2,8 @@ package types
 
 // ServiceRegisterInput
 type ServiceRegisterInput struct {
+	UserName   string `json:"UserName"`
+	Password   string `json:"password"`
 	DomainName string `json:"DomainName"`
 	IP         string `json:"IP"`
 	Port       string `json:"Port"`
